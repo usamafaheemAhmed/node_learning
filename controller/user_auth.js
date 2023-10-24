afecf17922184db4922dccdc3d85c5e4673a84fe
usamafaheemAhmed <117355964+usamafaheemAhmed@users.noreply.github.com>
2023-10-24 15:53:35 +0500

    uptil know

--- controller/user_auth.js
+++ controller/user_auth.js
@@ -5,11 +5,13 @@ const userDB = {
     
 
 const fsPromise = require('fs').promises;
-const fs = require('fs');
-const path = require('path');
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const handleAuth = async (req, res) => {
     // console.log(req.body);
@@ -19,15 +21,36 @@ const handleAuth = async (req, res) => {
         return res.status(400).json({ "message": "UserName and Password is required" });
     }
 
-    const duplicate = userDB.user.find(person => person.userName === userName);
-    // console.log("ma error hun",duplicate);
+    const foundUser = userDB.user.find(person => person.userName === userName);
+    // console.log("ma error hun",foundUser);
 
-    if (!duplicate) return res.status(400).json({ "message": "User does't exist" });
+    if (!foundUser) return res.status(400).json({ "message": "User does't exist" });
     // console.log("kunjum kunjum");
     
-    const match = await bcrypt.compare(pwd, duplicate.password);
+    const match = await bcrypt.compare(pwd, foundUser.password);
     
     if (match) {
+        
+        const accessToken = jwt.sign(
+            { "username": foundUser.userName },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: "20s"  // in production make it 5 min or 10min 
+            }
+        );
+
+        const refreshToken = jwt.sign(
+            { "username": foundUser.userName },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: "30min"  // in production make it 5 min or 10min 
+            }
+        );
+
+ 
+
+
+
         res.status(201).json({"success":`user ${userName} logged in `})
     } else {
         res.status(400).json({"massage":`password does't match`})
